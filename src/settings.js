0300cd3da41b8de33b8bc98eae72852d251e84c9
Jorin Liesse <liesse.jorin@gmail.com>
2024-06-27 23:03:18 +0200

    new start of the website

--- src/settings.js
+++
@@ -1,4 +0,0 @@
-export class Settings {
-    static drag = 0.95;
-    static moveSpeed = 0.001;
-}
--- src/shader.js
+++
@@ -1,5 +0,0 @@
-export async function fetchShader(url) {
-  const response = await fetch(url);
-  const shaderSource = await response.text();
-  return shaderSource;
-}
